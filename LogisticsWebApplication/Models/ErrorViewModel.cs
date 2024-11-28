631826cb6a4bc2510433019420c09f4738ef61f9
FelipeF-soares <felipe.fends@gmail.com>
2024-11-28 16:02:18 -0300

    1- Creating the project structure, including Views, Data Access, and Controllers, and adding dependencies.

---
+++ LogisticsWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogisticsWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
